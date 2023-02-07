ac00ddb7afdc1fcc558a3056ca10b50cc26ba26d
Kyohei-takiyama <takiyamakyohei@gmail.com>
2023-02-08 00:54:57 +0900

    ２進数を求める処理を追加

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 文字を反転
 const reverseString = (input) => {
   //   console.log(input);
   console.log(input.charAt(0));
@@ -8,4 +9,31 @@ const reverseString = (input) => {
   return reverseString(input.substring(1)) + input.charAt(0);
 };
 
-console.log(reverseString("hello"));
+console.log(reverseString("こんにちは"));
+
+// 回文か判定
+const isPalindrome = (input) => {
+  if (input.length === 0 || input.length === 1) {
+    return true;
+  }
+
+  if (input.charAt(0) == input.charAt(input.length - 1)) {
+    return isPalindrome(input.substring(1, input.length - 1));
+  }
+
+  return false;
+};
+
+console.log(isPalindrome("racecar"));
+
+// ２進数に変換
+const findBinary = (decimal, result) => {
+  if (decimal === 0) {
+    return result;
+  }
+
+  result = (decimal % 2) + result;
+  return findBinary(Math.floor(decimal / 2), result);
+};
+
+console.log(findBinary(233, ""));
